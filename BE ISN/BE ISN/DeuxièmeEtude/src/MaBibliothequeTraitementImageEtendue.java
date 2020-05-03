b77a3a62ca04edb1838b4e0e31de1a52f7fccc23
Leo <leolu@hotmail.fr>
2020-05-03 18:35:32 +0200

    Beginning of question 6

--- BE ISN/BE ISN/DeuxièmeEtude/src/MaBibliothequeTraitementImageEtendue.java
+++ BE ISN/BE ISN/DeuxièmeEtude/src/MaBibliothequeTraitementImageEtendue.java
@@ -38,78 +38,75 @@ import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 public class MaBibliothequeTraitementImageEtendue {
-	//Contient toutes les méthodes necessaires à la transformation des images
+	// Contient toutes les méthodes necessaires à la transformation des images
 
-
-	//Methode qui permet de transformer une matrice intialement au  format BGR au format HSV
-	public static Mat transformeBGRversHSV(Mat matriceBGR){
-		Mat matriceHSV=new Mat(matriceBGR.height(),matriceBGR.cols(),matriceBGR.type());
-		Imgproc.cvtColor(matriceBGR,matriceHSV,Imgproc.COLOR_BGR2HSV);
+	// Methode qui permet de transformer une matrice intialement au format BGR
+	// au format HSV
+	public static Mat transformeBGRversHSV(Mat matriceBGR) {
+		Mat matriceHSV = new Mat(matriceBGR.height(), matriceBGR.cols(), matriceBGR.type());
+		Imgproc.cvtColor(matriceBGR, matriceHSV, Imgproc.COLOR_BGR2HSV);
 		return matriceHSV;
 
 	}
 
-	//Methode qui convertit une matrice avec 3 canaux en un vecteur de 3 matrices monocanal (un canal par couleur)
+	// Methode qui convertit une matrice avec 3 canaux en un vecteur de 3
+	// matrices monocanal (un canal par couleur)
 	public static Vector<Mat> splitHSVChannels(Mat input) {
-		Vector<Mat> channels = new Vector<Mat>(); 
+		Vector<Mat> channels = new Vector<Mat>();
 		Core.split(input, channels);
 		return channels;
 	}
 
-	//Methode qui permet d'afficher une image sur un panel
-	public static void afficheImage(String title, Mat img){
-		MatOfByte matOfByte=new MatOfByte();
-		Highgui.imencode(".png",img,matOfByte);
-		byte[] byteArray=matOfByte.toArray();
-		BufferedImage bufImage=null;
-		try{
-			InputStream in=new ByteArrayInputStream(byteArray);
-			bufImage=ImageIO.read(in);
-			JFrame frame=new JFrame();
+	// Methode qui permet d'afficher une image sur un panel
+	public static void afficheImage(String title, Mat img) {
+		MatOfByte matOfByte = new MatOfByte();
+		Highgui.imencode(".png", img, matOfByte);
+		byte[] byteArray = matOfByte.toArray();
+		BufferedImage bufImage = null;
+		try {
+			InputStream in = new ByteArrayInputStream(byteArray);
+			bufImage = ImageIO.read(in);
+			JFrame frame = new JFrame();
 			frame.setTitle(title);
 			frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
 			frame.pack();
 			frame.setVisible(true);
 
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-
 	}
 
-	
-
-	//Methode qui permet d'extraire les contours d'une image donnee
+	// Methode qui permet d'extraire les contours d'une image donnee
 	public static List<MatOfPoint> ExtractContours(Mat input) {
 		// Detecter les contours des formes trouvées
 		int thresh = 100;
 		Mat canny_output = new Mat();
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		MatOfInt4 hierarchy = new MatOfInt4();
-		Imgproc.Canny( input, canny_output, thresh, thresh*2);
-
+		Imgproc.Canny(input, canny_output, thresh, thresh * 2);
 
 		/// Find extreme outer contours
-		Imgproc.findContours( canny_output, contours, hierarchy,Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		Imgproc.findContours(canny_output, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-		Mat drawing = Mat.zeros( canny_output.size(), CvType.CV_8UC3 );
+		Mat drawing = Mat.zeros(canny_output.size(), CvType.CV_8UC3);
 		Random rand = new Random();
-		for( int i = 0; i< contours.size(); i++ )
-		{
-			Scalar color = new Scalar( rand.nextInt(255 - 0 + 1) , rand.nextInt(255 - 0 + 1),rand.nextInt(255 - 0 + 1) );
-			Imgproc.drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, new Point() );
+		for (int i = 0; i < contours.size(); i++) {
+			Scalar color = new Scalar(rand.nextInt(255 - 0 + 1), rand.nextInt(255 - 0 + 1), rand.nextInt(255 - 0 + 1));
+			Imgproc.drawContours(drawing, contours, i, color, 1, 8, hierarchy, 0, new Point());
 		}
-		//afficheImage("Contours",drawing);
+		// afficheImage("Contours",drawing);
 
 		return contours;
 	}
 
-	//Methode qui permet de decouper et identifier les contours carrés, triangulaires ou rectangulaires. 
-	//Renvoie null si aucun contour rond n'a été trouvé.	
-	//Renvoie une matrice carrée englobant un contour rond si un contour rond a été trouvé
-	public static Mat DetectForm(Mat img,MatOfPoint contour) {
+	// Methode qui permet de decouper et identifier les contours carrés,
+	// triangulaires ou rectangulaires.
+	// Renvoie null si aucun contour rond n'a été trouvé.
+	// Renvoie une matrice carrée englobant un contour rond si un contour rond a
+	// été trouvé
+	public static Mat DetectForm(Mat img, MatOfPoint contour) {
 		MatOfPoint2f matOfPoint2f = new MatOfPoint2f();
 		MatOfPoint2f approxCurve = new MatOfPoint2f();
 		float[] radius = new float[1];
@@ -117,33 +114,35 @@ public class MaBibliothequeTraitementImageEtendue {
 		Rect rect = Imgproc.boundingRect(contour);
 		double contourArea = Imgproc.contourArea(contour);
 
-
 		matOfPoint2f.fromList(contour.toList());
 		// Cherche le plus petit cercle entourant le contour
 		Imgproc.minEnclosingCircle(matOfPoint2f, center, radius);
-		//System.out.println(contourArea+" "+Math.PI*radius[0]*radius[0]);
-		//on dit que c'est un cercle si l'aire occupé par le contour est à supérieure à  80% de l'aire occupée par un cercle parfait
-		if ((contourArea / (Math.PI*radius[0]*radius[0])) >=0.8) {
-			//System.out.println("Cercle");
-			Core.circle(img, center, (int)radius[0], new Scalar(255, 0, 0), 2);
-			Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
-			Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
-			Mat sign = Mat.zeros(tmp.size(),tmp.type());
+		// System.out.println(contourArea+" "+Math.PI*radius[0]*radius[0]);
+		// on dit que c'est un cercle si l'aire occupé par le contour est à
+		// supérieure à 80% de l'aire occupée par un cercle parfait
+		if ((contourArea / (Math.PI * radius[0] * radius[0])) >= 0.8) {
+			// System.out.println("Cercle");
+			Core.circle(img, center, (int) radius[0], new Scalar(255, 0, 0), 2);
+			Core.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+					new Scalar(0, 255, 0), 2);
+			Mat tmp = img.submat(rect.y, rect.y + rect.height, rect.x, rect.x + rect.width);
+			Mat sign = Mat.zeros(tmp.size(), tmp.type());
 			tmp.copyTo(sign);
 			return sign;
-		}else {
+		} else {
 
 			Imgproc.approxPolyDP(matOfPoint2f, approxCurve, Imgproc.arcLength(matOfPoint2f, true) * 0.02, true);
 			long total = approxCurve.total();
-			if (total == 3 ) { // is triangle
-				//System.out.println("Triangle");
-				Point [] pt = approxCurve.toArray();
-				Core.line(img, pt[0], pt[1], new Scalar(255,0,0),2);
-				Core.line(img, pt[1], pt[2], new Scalar(255,0,0),2);
-				Core.line(img, pt[2], pt[0], new Scalar(255,0,0),2);
-				Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
-				Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
-				Mat sign = Mat.zeros(tmp.size(),tmp.type());
+			if (total == 3) { // is triangle
+				// System.out.println("Triangle");
+				Point[] pt = approxCurve.toArray();
+				Core.line(img, pt[0], pt[1], new Scalar(255, 0, 0), 2);
+				Core.line(img, pt[1], pt[2], new Scalar(255, 0, 0), 2);
+				Core.line(img, pt[2], pt[0], new Scalar(255, 0, 0), 2);
+				Core.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+						new Scalar(0, 255, 0), 2);
+				Mat tmp = img.submat(rect.y, rect.y + rect.height, rect.x, rect.x + rect.width);
+				Mat sign = Mat.zeros(tmp.size(), tmp.type());
 				tmp.copyTo(sign);
 				return null;
 			}
@@ -157,69 +156,135 @@ public class MaBibliothequeTraitementImageEtendue {
 				Double minCos = cos.get(0);
 				Double maxCos = cos.get(cos.size() - 1);
 				boolean isRect = total == 4 && minCos >= -0.1 && maxCos <= 0.3;
-				boolean isPolygon = (total == 5 && minCos >= -0.34 && maxCos <= -0.27) || (total == 6 && minCos >= -0.55 && maxCos <= -0.45);
+				boolean isPolygon = (total == 5 && minCos >= -0.34 && maxCos <= -0.27)
+						|| (total == 6 && minCos >= -0.55 && maxCos <= -0.45);
 				if (isRect) {
 					double ratio = Math.abs(1 - (double) rect.width / rect.height);
-					//drawText(rect.tl(), ratio <= 0.02 ? "SQU" : "RECT");
-					//System.out.println("Rectangle");
-					Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
-					Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
-					Mat sign = Mat.zeros(tmp.size(),tmp.type());
+					// drawText(rect.tl(), ratio <= 0.02 ? "SQU" : "RECT");
+					// System.out.println("Rectangle");
+					Core.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+							new Scalar(0, 255, 0), 2);
+					Mat tmp = img.submat(rect.y, rect.y + rect.height, rect.x, rect.x + rect.width);
+					Mat sign = Mat.zeros(tmp.size(), tmp.type());
 					tmp.copyTo(sign);
 					return null;
 				}
 				if (isPolygon) {
-					//System.out.println("Polygon");
-					//drawText(rect.tl(), "Polygon");
+					// System.out.println("Polygon");
+					// drawText(rect.tl(), "Polygon");
 				}
-			}			
+			}
 		}
 		return null;
 
 	}
 
-	
-
 	public static double angle(Point a, Point b, Point c) {
-		Point ab = new Point( b.x - a.x, b.y - a.y );
-		Point cb = new Point( b.x - c.x, b.y - c.y );
+		Point ab = new Point(b.x - a.x, b.y - a.y);
+		Point cb = new Point(b.x - c.x, b.y - c.y);
 		double dot = (ab.x * cb.x + ab.y * cb.y); // dot product
 		double cross = (ab.x * cb.y - ab.y * cb.x); // cross product
 		double alpha = Math.atan2(cross, dot);
 		return Math.floor(alpha * 180. / Math.PI + 0.5);
 	}
 
-	
-	//methode à completer
-	public static double Similitude(Mat object,String signfile) {
+	// methode à completer
+	public static double Similitude(Mat object, String signfile) {
 
 		// Conversion du signe de reference en niveaux de gris et normalisation
 		Mat panneauref = Highgui.imread(signfile);
 		Mat graySign = new Mat(panneauref.rows(), panneauref.cols(), panneauref.type());
 		Imgproc.cvtColor(panneauref, graySign, Imgproc.COLOR_BGRA2GRAY);
 		Core.normalize(graySign, graySign, 0, 255, Core.NORM_MINMAX);
-		Mat signeNoirEtBlanc=new Mat();
-		
+		Mat signeNoirEtBlanc = new Mat();
 
-
-		//Conversion du panneau extrait de l'image en gris et normalisation et redimensionnement à la taille du panneau de réference
+		// Conversion du panneau extrait de l'image en gris et normalisation et
+		// redimensionnement à la taille du panneau de réference
 		Mat grayObject = new Mat(panneauref.rows(), panneauref.cols(), panneauref.type());
 		Imgproc.resize(object, object, graySign.size());
-		//afficheImage("Panneau extrait de l'image",object);
+		//afficheImage("Panneau extrait de l'image", object);
 		Imgproc.cvtColor(object, grayObject, Imgproc.COLOR_BGRA2GRAY);
 		Core.normalize(grayObject, grayObject, 0, 255, Core.NORM_MINMAX);
-		//Imgproc.resize(grayObject, grayObject, graySign.size());	
-		
-		
-		
-		//à compléter...
-		
-		return -1;
-		
-		
+		Imgproc.resize(grayObject, grayObject, graySign.size());
 
+		Core.normalize(graySign, graySign, 0, 255, Core.NORM_MINMAX);
+		
+		FeatureDetector orbDetector = FeatureDetector.create(FeatureDetector.ORB);
+		DescriptorExtractor orbExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+
+		MatOfKeyPoint objectKeypoints = new MatOfKeyPoint();
+		orbDetector.detect(grayObject, objectKeypoints);
+
+		MatOfKeyPoint signKeypoints = new MatOfKeyPoint();
+		orbDetector.detect(graySign, signKeypoints);
+
+		Mat objectDescriptor = new Mat(object.rows(), object.cols(), object.type());
+		orbExtractor.compute(grayObject, objectKeypoints, objectDescriptor);
+
+		Mat signDescriptor = new Mat(panneauref.rows(), panneauref.cols(), panneauref.type());
+		orbExtractor.compute(graySign, signKeypoints, signDescriptor);
+
+		MatOfDMatch matchs = new MatOfDMatch();
+
+		DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
+		matcher.match(objectDescriptor, signDescriptor, matchs);
+
+		System.out.println(matchs.dump());
+
+		Mat matchedImage = new Mat(panneauref.rows(), panneauref.cols() * 2, panneauref.type());
+		Features2d.drawMatches(object, objectKeypoints, panneauref, signKeypoints, matchs, matchedImage);
+
+		// return match calculer la distance 
+		return  -1;
+		
 
 	}
+/*
+	public static void question_6() {
+		// p33 du diapo open_cv
+		Mat sroadSign = Highgui.imread("ref30.jpg");
 
+		Mat sObject = new Mat();
+
+		Imgproc.resize(sroadSign, sObject, sroadSign.size());
+
+		Mat grayObject = new Mat(sObject.rows(), sObject.cols(), sObject.type());
+		Imgproc.cvtColor(sObject, grayObject, Imgproc.COLOR_BGRA2GRAY);
+
+		Core.normalize(grayObject, grayObject, 0, 255, Core.NORM_MINMAX);
+
+		Mat graySign = new Mat(sroadSign.rows(), sroadSign.cols(), sroadSign.type());
+		Imgproc.cvtColor(sroadSign, graySign, Imgproc.COLOR_BGRA2GRAY);
+
+		Core.normalize(graySign, graySign, 0, 255, Core.NORM_MINMAX);
+
+		FeatureDetector orbDetector = FeatureDetector.create(FeatureDetector.ORB);
+		DescriptorExtractor orbExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+
+		MatOfKeyPoint objectKeypoints = new MatOfKeyPoint();
+		orbDetector.detect(grayObject, objectKeypoints);
+
+		MatOfKeyPoint signKeypoints = new MatOfKeyPoint();
+		orbDetector.detect(graySign, signKeypoints);
+
+		Mat objectDescriptor = new Mat(sObject.rows(), sObject.cols(), sObject.type());
+		orbExtractor.compute(grayObject, objectKeypoints, objectDescriptor);
+
+		Mat signDescriptor = new Mat(sroadSign.rows(), sroadSign.cols(), sroadSign.type());
+		orbExtractor.compute(graySign, signKeypoints, signDescriptor);
+		
+		 * MatOfDMatch matchs = new MatOfDMatch();
+		 * 
+		 * DescriptorMatcher matcher =
+		 * DescriptorMatcher.create(DescriptorMatcher . BRUTEFORCE) matcher
+		 * .matchCobjectDescriptor, signDescriptor, matchs);
+		 * 
+		 * System. out.println(matchs.dump());
+		 * 
+		 * Mat matchedImage = new Mat(sroadSign.rows(), sroadSign.cols()*2,
+		 * sroadSign.type()) Features2d.drawMatches(sObject, objectKeypoints,
+		 * sroadSign, signKeypoints,
+		
+	}
+*/
 }
-
